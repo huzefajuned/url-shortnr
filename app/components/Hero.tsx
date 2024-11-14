20b69e25aed3d468cc54d70953852f777d2868b0
huzefa <huzefa@huzefas-MacBook-Air.local>
2024-11-15 01:51:20 +0530

    feat : paste fn added

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,9 +8,11 @@ import ShortedUrls from "./ShortedUrls";
 // import AutoSlider from "./AutoSlider";
 import CustomButton from "./ui/CustomButton";
 import Dev from "./Dev";
+import PasteIcon from "../images/paste.png";
 
 // import { useAuth } from "../context/auth.context";
 import ShareModel from "./ShareModel";
+import Image from "next/image";
 
 const Hero = () => {
   // const { user:<string> }  = useAuth();
@@ -18,6 +20,19 @@ const Hero = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [showModel, setShowModel] = useState<boolean>(false);
   const [newShortedUrl, setNewShortedUrl] = useState<string>("");
+
+  //  handling copy & paste 
+  const handlePaste = async () => {
+    try {
+      const clipboardText = await navigator.clipboard.readText();
+      setUrl(clipboardText);
+      toast.success('paste done!')
+
+    } catch (err) {
+      toast.error('Failed to paste')
+      console.error("Failed to paste: ", err);
+    }
+  };
 
   // api for short an URL
   const shortUrl = async (url: string) => {
@@ -70,19 +85,31 @@ const Hero = () => {
           </h1>
 
           <div className="flex flex-col w-full  sm:flex-row sm:p-2 items-center  gap-0 sm:gap-5 mt-6  sm:mt-10  p-2">
+            {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+
             <input
               onChange={(e) => setUrl(e.target.value)}
               type="text"
+              value={url}
               placeholder="Paste your URL here"
               className="flex-grow  w-full px-2 py-4  sm:px-2 sm:py-3  rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+          
 
-            <CustomButton
-              btnTitle="Shorten"
-              onClick={() => onSubmit_(url)}
-              customStyle="p-3 sm:p-3 text-md  mt-4 sm:mt-0 "
-            />
+            <div className=" flex flex-row  gap-5 items-center justify-between">
+              <Image
+                onClick={()=>handlePaste()}
+                src={PasteIcon}
+                alt="Paste Icon"
+                className="w-10 h-10 mr-1  cursor-pointer" 
+              />
+
+              <CustomButton
+                btnTitle="Shorten"
+                onClick={() => onSubmit_(url)}
+                customStyle="p-3 sm:p-3 text-md  mt-4 sm:mt-0 "
+              /> 
+            </div>
           </div>
         </div>
         {/* show model */}
---
+++ app/components/Paste.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Paste from '../images/paste.png'
+// biome-ignore lint/suspicious/noRedeclare: <explanation>
+const Paste = () => {
+  return (
+    <div>Paste</div>
+  )
+}
+
+export default Paste
